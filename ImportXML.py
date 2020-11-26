e8b6216a8dc63c95a4c7e04531af20badd7b3341
ponte1010 <bon_heur303@yahoo.co.jp>
2020-11-26 23:01:48 +0900

    [add]ファイル選択ダイアログの表示

--- ImportXML.py
+++ ImportXML.py
@@ -1,3 +1,9 @@
-import tkinter, tkinter.filedialog, tkinter.messagebox
+# -*- coding: utf8 -*-
+import tkinter.messagebox
+import tkinter.filedialog
 
+# メッセージダイアログの表示
 tkinter.messagebox.showinfo('xmlファイルの選択','処理するxmlファイルを選択してください！')
+
+# ファイル選択ダイアログの表示
+file_path = tkinter.filedialog.asksaveasfilename()
